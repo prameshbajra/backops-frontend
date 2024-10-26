d787a56f33e504cb420798859b1672923b782920
prameshbajra <pe.messh@gmail.com>
2024-10-26 21:27:38 +0545

    Animations added

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,19 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { from, mergeMap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { AuthService } from '../../services/auth.service';
 import { FileService } from '../../services/file.service';
 import { LogoutComponent } from "../logout/logout.component";
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-file-upload',
   standalone: true,
-  imports: [LogoutComponent],
+  imports: [CommonModule, LogoutComponent],
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.css',
-  providers: [FileService]
+  providers: [FileService],
+  animations: [
+    trigger('slideButton', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('300ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'translateX(100%)', opacity: 0 }))
+      ])
+    ]),
+    trigger('slideUpload', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('300ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'translateX(-100%)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class FileUploadComponent implements OnInit {
+
+  totalFiles: number = 0;
+  completedFiles: number = 0;
+  isUploading: boolean = false;
 
   constructor(private fileUploadService: FileService, private authService: AuthService) { }
 
@@ -22,16 +48,23 @@ export class FileUploadComponent implements OnInit {
 
   async onFileSelected(event: any) {
     const files: File[] = event.target.files;
+    this.totalFiles = files.length;
+    this.completedFiles = 0;
+    this.isUploading = true;
+
     const concurrentUploads = environment.CONCURRENT_UPLOADS;
 
     from(files).pipe(mergeMap(file => this.uploadSingleFile(file), concurrentUploads)).subscribe({
       next: (result) => {
+        this.completedFiles++;
         console.log('File uploaded successfully', result);
       },
       error: (error) => {
+        alert('Error during file upload. Please try again.');
         console.error('Error during file upload', error);
       },
       complete: () => {
+        this.isUploading = false;
         console.log('All files uploaded');
       }
     });
