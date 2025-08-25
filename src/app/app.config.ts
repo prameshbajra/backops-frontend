9c79e700ea5eaa8d9082e9f03cc95c941d308ba7
prameshbajra <pe.messh@gmail.com>
2025-08-25 21:34:55 +0200

    Add application icons and web manifest for PWA support
    - Added icon files (72x72 and 96x96) to the public/icons directory.
    - Created a web manifest file to define the application metadata, including name, theme color, background color, and icon specifications for various sizes.
    - Ensured icons are marked as "maskable" for better display on different devices.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -6,6 +6,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { responseInterceptor } from './interceptors/response.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,9 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([
         authInterceptor, responseInterceptor
       ])
-    ),
+    ), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
